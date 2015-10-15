16db57ac6e6109d8c6a6fa7085574c8fc709e2af
Utilisateur <wow.lolomekiss@hotmail.fr>
2015-10-15 12:16:43 +0200

    modif exo3

--- td2/exo3.c
+++ td2/exo3.c
@@ -27,14 +27,53 @@ void affiche_liste (Liste l) {
 	}
 }
 
-Liste ajout_début ( Liste l, int v){
-	Element elt* = malloc(sizeof(Element));
+Liste ajout_debut (Liste l, int v){
+	Element* elt = malloc(sizeof(Element));
 	elt->val = v;
 	elt->suivant = l;
 	return elt;
 }
 
+Liste ajout_fin (Liste l, int v) {
+	Liste tmp = l;
+	Element* elt = malloc(sizeof(Element));
+	elt->val = v;
+	elt->suivant = NULL;
+	if (l==NULL) return elt;
+	while (l->suivant !=NULL) l = l->suivant;
+	l->suivant = elt;
+	return tmp;
+}
+
+int test_trie (Liste l){
+	if (l == NULL) return 1;
+	if (l->val > (l->suivant)->val) return 0;
+	else return test_trie(l->suivant);
+}
+
+Liste ajout_trie(Liste l,int v) {
+	if (test_trie (l)){
+		Liste tmp = l;
+		Element* elt = malloc(sizeof(Element));
+		elt->val = v;
+		if (l->val >= v) return ajout_debut (l,v); 			// le 1er element est inf
+		while (l->suivant != NULL && (l->suivant)->val <v ) l = l->suivant;
+		elt->suivant = l->suivant;
+		l->suivant = elt;
+		return tmp;
+	}
+	else return ajout_debut(l,v);
+}
+
 int main () {
-	
+	Liste liste;
+	liste = init_liste_vide();
+	affiche_liste (liste);
+	liste = ajout_debut(liste, 2);
+	affiche_liste (liste);
+	liste = ajout_fin (liste, 6);
+	affiche_liste(liste);
+	liste = ajout_trie (liste, 1);
+	affiche_liste(liste);
 	return 0;
 }
